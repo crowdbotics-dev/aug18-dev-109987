e0f4b99f170180bd482eb9f00cbe61d6799e49d2
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-18 12:31:49 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Mithiles
+admin.site.register(Mithiles)
 
 # Register your models here.
--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Mithiles,Mithiles,Mithiles
+from .serializers import MithilesSerializer,MithilesSerializer,MithilesSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,8 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+class MithilesViewSet(viewsets.ModelViewSet):
+    serializer_class = MithilesSerializer
+    authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication)
+    queryset = Mithiles.objects.all()
